24f0f1918e45497f8fc94210ef8239dda719c556
dimpradana <dimasadi180818@gmail.com>
2025-10-15 21:46:08 +0700

    Modul 5: Integrasi DRF, tambah Serializer & List/RetrieveAPIView untuk warga

--- warga/urls.py
+++ warga/urls.py
@@ -8,7 +8,8 @@ from .views import (
     WargaUpdateView,  
     WargaDeleteView,
     PengaduanUpdateView, 
-    PengaduanDeleteView  
+    PengaduanDeleteView
+   
 )
 
 urlpatterns = [
--- warga/views.py
+++ warga/views.py
@@ -4,6 +4,10 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Warga, Pengaduan
 from .forms import WargaForm
 from .forms import WargaForm, PengaduanForm 
+from django.urls import reverse_lazy
+from rest_framework.generics import ListAPIView
+from .serializers import WargaSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 class WargaListView(ListView):
     model = Warga
@@ -56,3 +60,11 @@ class PengaduanDeleteView(DeleteView):
     model = Pengaduan
     template_name = 'warga/pengaduan_confirm_delete.html'
     success_url = reverse_lazy('pengaduan-list')
+
+class WargaListAPIView(ListAPIView):
+    queryset = Warga.objects.all()
+    serializer_class = WargaSerializer
+
+class WargaDetailAPIView(RetrieveAPIView):
+    queryset = Warga.objects.all()
+    serializer_class = WargaSerializer
